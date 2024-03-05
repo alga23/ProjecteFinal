559b95456d910c29c982e331df17e9b80c4a9544
JonathanValle12 <jonathin_2002@outlook.es>
2024-03-05 15:13:25 +0100

    Configuración backend y creación de modelos de user y las interacción hacia una publicación

---
+++ backend/routes/follow.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Rutas para los follows
+
+
+
+//Exportar todas las rutas
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Rutas para el Usuario
+
+
+
+//Exportar todas las rutas
+module.exports = router;
