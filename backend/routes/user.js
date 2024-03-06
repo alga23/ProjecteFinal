41faf15000a2dd5881a3a86fcd64fcea49bec685
JonathanValle12 <jonathin_2002@outlook.es>
2024-03-06 15:52:04 +0100

    arreglos y modificación + refactorización de codigo y validaciónes personalizadas en el backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const UserController = require('../controllers/user');
 
 // Rutas para el Usuario
-
-
+router.post('/registro', UserController.register);
+router.post('/login', UserController.login);
 
 //Exportar todas las rutas
 module.exports = router;
