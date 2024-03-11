9ec880f94d7e03e5566fe4430e95cb82cd786d7e
alga23 <algarcac3@gmail.com>
2024-03-11 15:47:04 +0100

    Creada mi rama

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,13 @@
 import Welcome from './src/views/user/Welcome';
 import Login from './src/views/user/Login';
 import { NavigationContainer } from '@react-navigation/native';
+import Registro from './src/views/user/Registro';
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Login />
+      <Registro/>
     </NavigationContainer>
   );
 }
