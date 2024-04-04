da61a08db1e1bade3d5956188bd1a515b3213e39
Daniel C <Danielcontaf1996@gmail.com>
2024-04-04 16:20:24 +0200

    create post funcional y no array de users en post model

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,20 +3,24 @@ const User = require('../models/user')
 
 const createPost = async (req, res) => {
     //Check for content
-    if (!req.body.content) {
-        return res.status(400).send("No content found.")
+    try {
+        if (!req.body.content) {
+            return res.status(400).send("No content found.")
+        }
+        const user = req.user
+
+        const post = new Post({
+            content: req.body.content,
+            user_id: user.id, //user from the authMiddleware
+        })
+        await post.save();
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(500).send({
+            status: "error",
+            message: "Error en el servidor: " + error
+        })
     }
-    const user = req.user
-
-    const post = new Post({
-        content: req.body.content,
-        user_id: user.id, //user from the authMiddleware
-    })
-    user.posts_id.push(post.id)
-
-    await post.save();
-    await user.save();
-    res.status(201).json(post);
 }
 
 module.exports = {
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const auth = require('../middleware/auth')
 const router = express.Router();
+const postController = require('../controllers/post')
 
 // Rutas para el post
 router.post("/create", auth, postController.createPost)
