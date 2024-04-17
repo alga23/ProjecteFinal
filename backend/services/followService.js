0fdcfef4011df9c72ed1f7a96d01f92aa3f19a06
Daniel C <145220832+Daniel-C96@users.noreply.github.com>
2024-04-17 15:08:21 +0200

    Daniel c (#70)
    * responses v1
    
    * viewProfile v1 sin follows ni followers aun

--- backend/services/followService.js
+++ backend/services/followService.js
@@ -1,15 +1,15 @@
 const Follow = require('../models/follow');
 
 // Metodo para obtener todos los ids de usuarios seguidos y los que me siguen
-const followindUserIds = async(identityUser) => {
+const followindUserIds = async (identityUser) => {
 
     try {
-        
+
         // Buscar todos los ids de usuarios que estoy siguiendo
-        const following = await Follow.find({"user": identityUser}).select({"follower": 1, "_id": 0}).exec();
+        const following = await Follow.find({ "user": identityUser }).select({ "follower": 1, "_id": 0 }).exec();
 
         // Buscar todos los ids de usuarios que me siguen
-        const follower = await Follow.find({"follower": identityUser}).select({"user": 1, "_id": 0}).exec();
+        const follower = await Follow.find({ "follower": identityUser }).select({ "user": 1, "_id": 0 }).exec();
 
         const followingClean = [];
 
@@ -30,8 +30,8 @@ const followindUserIds = async(identityUser) => {
             follower: followerClean
         }
 
-    }catch(error) {
-        
+    } catch (error) {
+
         return res.status(500).send({
             status: "error",
             message: "Error en el servidor de Follows: ", error
@@ -39,6 +39,15 @@ const followindUserIds = async(identityUser) => {
     }
 }
 
+const followsUser = async (identityUser) => {
+    // Buscar todos los ids de usuarios que estoy siguiendo
+    const following = await Follow.find({ "user": identityUser }).select({ "follower": 1, "_id": 0 }).exec();
+
+    // Buscar todos los ids de usuarios que me siguen
+    const follower = await Follow.find({ "follower": identityUser }).select({ "user": 1, "_id": 0 }).exec();
+}
+
 module.exports = {
-    followindUserIds
+    followindUserIds,
+    followsUser
 }
