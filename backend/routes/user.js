f4045f350dadb3994f39c387ffc5bfce92e19647
alga23 <algarcac3@gmail.com>
2024-05-06 15:21:53 +0200

    Subido nueva version del users y del routers para poder editar usuario

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const UserController = require('../controllers/user');
-const Auth = require('../middleware/auth');
+const auth = require('../middleware/auth');
 
 // Rutas para el Usuario
 router.post('/registro', UserController.register);
 router.post('/login', UserController.login);
 router.get('/profile/:userId', UserController.viewUserProfile);
+router.delete('/delete/:userId', UserController.deleteUser);
+router.put('/editUser/', auth, UserController.editUser);
+
+
 
 //Exportar todas las rutas
 module.exports = router;
