49084a5365de86ed3d6ace150f6d5739eaa85af7
Daniel C <Danielcontaf1996@gmail.com>
2024-05-09 16:05:53 +0200

    translation v1

---
+++ frontend/src/languages/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import es from './es.json';
+import en from './en.json';
+import { getLocales } from 'expo-localization';
+
+const locales = getLocales();
+let phoneLocale = 'en'; // Idioma predeterminado en caso de que no se detecte ninguna configuración regional.
+
+if (locales && locales.length > 0) {
+    phoneLocale = locales[0].languageCode; // Obtén el primer idioma de la lista
+}
+i18n
+    .use(initReactI18next)
+    .init({
+        compatibilityJSON: 'v3',
+        resources: {
+            en: {
+                translation: en,
+            },
+            es: {
+                translation: es,
+            },
+        },
+        lng: phoneLocale, // Set detected locale as initial language
+        fallbackLng: 'en',
+    });
+
+export default i18n;
